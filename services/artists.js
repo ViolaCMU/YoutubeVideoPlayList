288223cc4fa7d17685edb9bd740e6225a858be54
Jingxuan Hu <Xuan@Jingxuans-MBP.wv.cc.cmu.edu>
2016-03-07 17:36:29 -0500

    add few artists

--- services/artists.js
+++ services/artists.js
@@ -1,4 +1,4 @@
 // store artists list
-var artists = ['Elton John', 'Stevie Wonder', 'Frank Sinatra', 'Louis Armstrong'];
+var artists = ['Elton John', 'Stevie Wonder', 'Frank Sinatra', 'Louis Armstrong', 'Stanley Turrentine', 'Adele', 'Maroon 5', 'Whitney Houston'];
 
 module.exports = artists;
